77e99010bace4f74b9da15d3551c1b6c36b41be7
AlexaKHead <akh00002@mix.wvu.edu>
2023-01-21 00:50:50 -0500

    Alexa Head Task 02 upload a

---
+++ Task_02/ProgressCodeTask02.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/lexik/Evolution/Tasks/Task_02")
+#
+Data1 <- read.csv('http://jonsmitchell.com/data/beren.csv', stringsAsFactors=F)
+Data2 <- read.csv('http://jonsmitchell.com/data/cyrus.csv, stringsAsFactors=F')
+#
+write.csv(Data1, 'rawdata.csv', quote=F)
